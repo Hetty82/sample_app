addbe7134b32ee0f29ae7ccebaf23b890d8b2df5
Hetty de Vries <devries@hetty82.nl>
2014-02-24 21:51:55 +0100

    Add user/new form and create action

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,6 +8,32 @@ describe "User Pages" do
 
     it { should have_selector('h1', text: 'Sign Up') }
     it { should have_selector('title', text: full_title('Sign Up')) }
+
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Create my account"}
+
+    describe "with invalid data" do
+      it "should not create a user" do
+        expect { click_button submit }.to_not change(User, :count)
+      end
+
+    end
+
+    describe "with valid data" do
+      before do
+        fill_in "Name", with: "Mickey Mouse"
+        fill_in "Email", with: "mickey@mouse.com"
+        fill_in "Password", with: "password"
+        fill_in "Confirmation", with: "password"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count)
+      end
+    end
   end
 
   describe "Profile page" do
