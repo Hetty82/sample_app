8fcd181e9dba15705ff87239d5daba1a30d2773e
Hetty de Vries <devries@hetty82.nl>
2014-03-17 17:23:37 +0100

    Add edit for users and authorization for edit and update

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
   has_secure_password # moved it here for ordering error messages
-  validates :password, length: {minimum: 6}
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
 private
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,7 +29,7 @@ describe "User Pages" do
         it { should have_error_message("Name can't be blank") }
         it { should have_error_message("Email can't be blank") }
         it { should have_error_message("Email is invalid") }
-        it { should have_error_message("Password is too short (minimum is 6 characters)") }
+        it { should have_error_message("Password is too short") }
         it { should have_error_message("Password confirmation can't be blank") }
       end
     end
@@ -58,6 +58,47 @@ describe "User Pages" do
 
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_selector('h1', text: "Update your profile") }
+      it { should have_link('change photo', href: "http://gravatar.com/emails") }
+    end
+
+    describe "with invalid data" do
+      before { click_button "Save changes" }
+
+      it { should have_error_message("Password is too short") }
+      it { should have_error_message("Password confirmation can't be blank") }
+    end
+
+    describe "with valid data" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Confirmation", with: user.password_confirmation
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_success_message("Your profile is updated") }
+      it { should have_link('Sign out', href: signout_path) }
+
+      specify { user.reload.name.should == new_name }
+      specify { user.reload.email.should == new_email }
+
+    end
   end
 
 end
